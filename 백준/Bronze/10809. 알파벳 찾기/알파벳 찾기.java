80e8d1dc3a6f42dcb0a9d6036a7e85b7c6af6ce7
임토웅 <104752202+limtowoong@users.noreply.github.com>
2023-05-30 21:03:36 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18344 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int a[] = new int[26];
-        for(int i=0; i<26; i++){
-            a[i] = -1;
+        String a = sc.next();
+        int arr[] = new int[26];
+        int arr2[] = new int[26];
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = -1;
+            arr2[i] = i;
         }
 
-        String b = sc.nextLine();
-
-        for(int i=0; i<b.length(); i++){
-            char ch = b.charAt(i);
-
-            if(a[ch - 97] == -1){
-                a[ch - 97] = i;
+        for(int i=0; i<a.length(); i++){
+            for(int j=0; j<arr.length; j++){
+                char ch = a.charAt(i);
+                if(ch-97 == arr2[j] && arr[j] == -1){
+                    arr[ch - 97] = i;
+                }
             }
         }
-        for( int c : a ){
-            System.out.print(c+" ");
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
         }
     }
 }
